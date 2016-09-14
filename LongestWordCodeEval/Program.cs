d73be426f90a0fce0bd58cd8631f488dc433d572
dtheobald58 <dtheobald58@yahoo.com>
2016-09-14 09:16:08 -0400

    initial commit

---
+++ LongestWordCodeEval/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWordCodeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "awesome lobster on fire";
+
+            string[]splitLine = line.Split(' ');
+            string longWord = "";
+
+            for (int i = 0; (i <= splitLine.Length-1); i++)
+            {
+                if (splitLine[i].Length > longWord.Length)
+                {
+                    longWord = splitLine[i];
+                }
+            }
+            Console.WriteLine(longWord);
+        }
+    }
+}
